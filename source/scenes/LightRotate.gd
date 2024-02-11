f8fd537cf4a884a7731a3092ba267f5e9d222756
John Meadow <leszek.nowak@uj.edu.pl>
2024-02-11 17:50:41 +0100

    added saturn

--- source/scenes/LightRotate.gd
+++ source/scenes/LightRotate.gd
@@ -4,10 +4,14 @@ extends Node2D
 var angle:= 0.0
 @onready var sprite = $PlanetSprite
 @onready var marker = $PlanetSprite/LightSource
+@export var ring:Sprite2D
+
+var light_direction := Vector3.ONE
 
 func _process(delta):
 	angle += delta * 0.5
 	marker.position = Vector2.RIGHT.rotated(angle) * 2.0
-	var light_direction:= Vector3(marker.position.x, marker.position.y, 1.0).normalized()
+	light_direction = Vector3(marker.position.x, marker.position.y, 1.0).normalized()
 	sprite.material.set_shader_parameter("light_direction", light_direction)
-	
+	if ring:
+		ring.material.set_shader_parameter("light_direction", light_direction)
